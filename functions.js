b834c35d74bb5aef600fed9de066ac027906cf58
yaguchiharuto <haruto.yaguchi.7c@stu.hosei.ac.jp>
2024-11-25 02:27:59 +0900

    add infomation

--- functions.js
+++ functions.js
@@ -2,9 +2,9 @@
 const chapters = document.querySelectorAll('.chapter');
 
 // 取得した各要素に対してアニメーションを適用
-chapters.forEach((text) => {
+chapters.forEach((content) => {
   gsap.fromTo(
-    text, // アニメーションを適用する要素
+    content, // アニメーションを適用する要素
     {autoAlpha: 0, y: 50}, // アニメーション開始時の状態（透明度0、y方向に50px下にずらす）
     {
       duration: 2, // アニメーションの時間（秒）
@@ -12,12 +12,13 @@ chapters.forEach((text) => {
       y: 0, // アニメーション終了時のy方向の位置（0でオリジナルの位置）
       ease: 'power2.out', // イージングの種類（power2.outで終わりに向けて徐々にスピードが落ちる）
       scrollTrigger: {
-        trigger: text, // スクロールのトリガーとなる要素
-        start: 'top 80%', // アニメーションが開始するスクロール位置（チャプター要素の上端が画面の80%の位置に来たら開始）
+        trigger: content, // スクロールのトリガーとなる要素
+        start: 'top 100%', // アニメーションが開始するスクロール位置（チャプター要素の上端が画面の80%の位置に来たら開始）
         toggleActions: 'play none none none', // スクロールによるアニメーションの動作（playで再生、noneで何もしない,https://codepen.io/GreenSock/pen/LYVKWGoを参照）
-        once: true, // アニメーションを一度だけ実行
+        once: false, // アニメーションを一度だけ実行
         
-      }
+      },
+      stagger: 0.2,
     }
   );
 });
